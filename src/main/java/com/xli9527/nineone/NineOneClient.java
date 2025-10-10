2afc518e334b7596da80ab03e142124aebc37d96
xli9527 <buli_xli@163.com>
2025-10-10 16:59:08 +0800

    Initial commit

---
+++ src/main/java/com/xli9527/nineone/NineOneClient.java
@@ -0,0 +1,10 @@
+package com.xli9527.nineone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NineOneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/xli9527/nineone/NineOneDataGenerator.java
@@ -0,0 +1,11 @@
+package com.xli9527.nineone;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NineOneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
