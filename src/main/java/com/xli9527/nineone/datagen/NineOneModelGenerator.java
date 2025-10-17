3ddc3ff1fa9867e8d8ae740266108b1a3894f8e1
xli9527 <buli_xli@163.com>
2025-10-17 19:58:25 +0800

    new

--- src/main/java/com/xli9527/nineone/datagen/NineOneModelGenerator.java
+++ src/main/java/com/xli9527/nineone/datagen/NineOneModelGenerator.java
@@ -1,9 +1,11 @@
 package com.xli9527.nineone.datagen;
 
+import com.xli9527.nineone.item.ModItems;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.client.data.BlockStateModelGenerator;
 import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.Models;
 
 public class NineOneModelGenerator extends FabricModelProvider {
 
@@ -18,6 +20,10 @@ public class NineOneModelGenerator extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-
+        itemModelGenerator.register(ModItems.KNIFE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.MEN_ZI, Models.GENERATED);
+        itemModelGenerator.register(ModItems.MEN_ZI_PIECE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.NINEONE_LOGO, Models.GENERATED);
+        itemModelGenerator.register(ModItems.STARCH, Models.GENERATED);
     }
 }
