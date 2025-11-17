d1a09cf0ad0e67e853bb43f51814caa751d71c13
xli9527 <buli_xli@163.com>
2025-11-17 16:50:20 +0800

    new

--- src/main/java/com/xli9527/nineone/item/ModItemGroups.java
+++ src/main/java/com/xli9527/nineone/item/ModItemGroups.java
@@ -1,5 +1,6 @@
 package com.xli9527.nineone.item;
 
+import com.xli9527.nineone.NineOne;
 import com.xli9527.nineone.item.custom.itemgroup.NineOneGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
@@ -9,12 +10,14 @@ import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
-import static net.minecraft.registry.Registries.ITEM_GROUP;
+import static net.minecraft.registry.RegistryKeys.ITEM_GROUP;
+
 
 public final class ModItemGroups {
+
     public static final ItemGroup TEST_GROUP = FabricItemGroup.builder()
             .icon(() -> new ItemStack(ModItems.NINEONE_LOGO))
-            .displayName(Text.translatable("itemGroup.tutorial.test_group"))
+            .displayName(Text.translatable("itemGroup.nineone.test_group"))
             .entries((context, entries) -> {
                 entries.add(ModItems.NINEONE_LOGO);
             })
@@ -22,11 +25,18 @@ public final class ModItemGroups {
 
 
     public static void ADD_ITEMS(){
-        NineOneGroup.registerItemsToGroup();
+
+    }
+
+    public void RegisterItemGroups(String id, ItemGroup group) {
+
     }
 
     public static void initialize() {
-
+        Registry.register(Registries.ITEM_GROUP, Identifier.of(NineOne.MOD_ID,"test_group"),TEST_GROUP);
+        NineOneGroup.registerItemsToGroup();
+        NineOne.LOGGER.info("Initializing NineOne Mod ItemGroups");
     }
 
 }
+
--- src/main/java/com/xli9527/nineone/item/custom/itemgroup/NineOneGroup.java
+++ src/main/java/com/xli9527/nineone/item/custom/itemgroup/NineOneGroup.java
@@ -1,13 +1,18 @@
 package com.xli9527.nineone.item.custom.itemgroup;
 
+import com.xli9527.nineone.NineOne;
+import com.xli9527.nineone.item.ModItemGroups;
 import com.xli9527.nineone.item.ModItems;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 
 public class NineOneGroup {
 
     public static void registerItemsToGroup() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
+        ItemGroupEvents.modifyEntriesEvent(RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of("test_group"))).register(content -> {
             content.add (ModItems.KNIFE);
             content.add (ModItems.MEN_ZI);
             content.add (ModItems.MEN_ZI_PIECE);
